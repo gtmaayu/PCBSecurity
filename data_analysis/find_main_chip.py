9b3af4d12c37765286178d6921327ac15463cd0d
EmmaB3 <emma.l.bethel@gmail.com>
2024-04-12 14:41:05 -0400

    now looking at right corner

--- data_analysis/find_main_chip.py
+++ data_analysis/find_main_chip.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
 from itertools import combinations
-from imgproc_helpers import load_images, average_image, is_valid
 
-VID_DIR = 'april3_microphoneBoardE_0'
+IMG_PATH = '/Users/emmabethel/Documents/Classes/Capstone/PCBSecurity/RecordedVideos_april10_baselineBoardA_0_raw_screenshot.png'
 
 
 def find_intersection(l1, l2):
@@ -47,9 +46,8 @@ def find_center_chip(img):
     # _,thresh = cv2.threshold(img,135,255,cv2.THRESH_TOZERO)
     # cv2.imshow("thresholded", thresh)
    
-    edges = cv2.Canny(img, 100, 200)
+    edges = cv2.Canny(img, 50, 200)
     # contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # Detect lines using Hough Line Transform
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=25, minLineLength=15, maxLineGap=5)
 
     # cv2.imshow("hello", edges)
@@ -72,38 +70,41 @@ def find_center_chip(img):
     sorted_indices = np.argsort(absolute_diff)
     d = 0
     i = 0
-    while d < 10:
-        closest_to_1_indices = sorted_indices[i:i + 2]  # Select the 4 lines with absolute slope values closest to 1
+    while d < 30:
+        closest_to_1_indices = sorted_indices[i:i + 2]  # Select the 2 lines with absolute slope values closest to 1
         d = calculate_y_intercept_difference(filtered_lines[closest_to_1_indices[0]], filtered_lines[closest_to_1_indices[1]])
         i += 1
 
     sorted_indices_neg_1 = np.argsort(absolute_diff_neg_1)
     d = 0
     i = 0
-    while d < 10:
-        closest_to_neg_1_indices = sorted_indices_neg_1[i:i + 2]  # Select the 4 lines with absolute slope values closest to 1
+    while d < 30:
+        closest_to_neg_1_indices = sorted_indices_neg_1[i:i + 2]  # Select the 2 lines with absolute slope values closest to 1
         d = calculate_y_intercept_difference(filtered_lines[closest_to_neg_1_indices[0]], filtered_lines[closest_to_neg_1_indices[1]])
         i += 1
 
     final_lines = np.concatenate((closest_to_neg_1_indices, closest_to_1_indices))
 
     # Draw selected lines on the original image
-    # for i in range(len(filtered_lines)):
-    #     if i in final_lines:
-    #         color = (0, 0, 255)
-    #     else:
-    #         color = (0, 255, 0)
+    for i in range(len(filtered_lines)):
+        if i in final_lines:
+            color = (0, 0, 255)
+        else:
+            color = (0, 255, 0)
 
-    #     line = filtered_lines[i]
-    #     x1, y1, x2, y2 = line
-    #     cv2.line(color_img, (x1, y1), (x2, y2), color, 1)
+        line = filtered_lines[i]
+        x1, y1, x2, y2 = line
+        cv2.line(color_img, (x1, y1), (x2, y2), color, 1)
 
     corners = []
     for i, j in list(combinations(final_lines, 2)):
         if slopes[i] * slopes[j] < 0:  # different signs
             corners.append(find_intersection(filtered_lines[i], filtered_lines[j]))
 
-            # cv2.circle(color_img, corners[-1], 2, (255, 0, 0), -1)
+            cv2.circle(color_img, corners[-1], 2, (255, 0, 0), -1)
+
+    # cv2.imshow("hello", color_img)
+    # cv2.waitKey(0)
 
     corners.sort(key=lambda x: x[0])
     if corners[1][1] > corners[2][1]:
@@ -113,17 +114,18 @@ def find_center_chip(img):
 
     return corners
 
+def find_atmega_right_corner(img):
+    corners = find_center_chip(img)
+
+    for x,y in corners:
+        if 540 < x < 560 and 350 < y < 400:
+            return (x,y) 
+
 if __name__ == "__main__":
     # figuring out how much to shift video 1 over by to put the arduino in the same spot as in video 2
-    jpg1 = load_images(f'{VID_DIR}/preview', 'frame_{:06d}.jpg', read_flag=cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(IMG_PATH, cv2.IMREAD_GRAYSCALE)
 
-    avg1 = average_image(jpg1, lambda img: is_valid(img, 29))
 
-    corners = find_center_chip(avg1)
+    corner = find_atmega_right_corner(img)
 
-    rightmost = np.array(corners[3])
-
-    with open(f'{VID_DIR}/alignment.txt', 'w') as file:
-        # Write a line to the file
-        for corner in corners:
-            file.write(f'{corner[0]},{corner[1]}\n')
+    print(corner)
